c7df4b1b7691c65e5564a5c6579f5bea490a88c3
Norman Teibert <norman.teibert@gmail.com>
2017-06-11 10:08:03 +0200

    #1

---
+++ src/main/java/homework13/Movie_actor.java
@@ -0,0 +1,41 @@
+package homework13;
+
+public class Movie_actor {
+
+    private Integer actor_id;
+    private Integer film_id;
+    private String last_update;
+
+    public Integer getActor_id() {
+        return actor_id;
+    }
+
+    public void setActor_id(Integer actor_id) {
+        this.actor_id = actor_id;
+    }
+
+    public Integer getFilm_id() {
+        return film_id;
+    }
+
+    public void setFilm_id(Integer film_id) {
+        this.film_id = film_id;
+    }
+
+    public String getLast_update() {
+        return last_update;
+    }
+
+    public void setLast_update(String last_update) {
+        this.last_update = last_update;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie_actor{" +
+                "actor_id=" + actor_id +
+                ", film_id=" + film_id +
+                ", last_update='" + last_update + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/homework13/Payment.java
@@ -0,0 +1,71 @@
+package homework13;
+
+public class Payment {
+
+    private Integer payment_id;
+    private Integer customer_id;
+    private Integer staff_id;
+    private Integer rental_id;
+    private Double amount;
+    private String payment_date;
+
+    public Integer getPayment_id() {
+        return payment_id;
+    }
+
+    public void setPayment_id(Integer payment_id) {
+        this.payment_id = payment_id;
+    }
+
+    public Integer getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(Integer customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public Integer getStaff_id() {
+        return staff_id;
+    }
+
+    public void setStaff_id(Integer staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    public Integer getRental_id() {
+        return rental_id;
+    }
+
+    public void setRental_id(Integer rental_id) {
+        this.rental_id = rental_id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getPayment_date() {
+        return payment_date;
+    }
+
+    public void setPayment_date(String payment_date) {
+        this.payment_date = payment_date;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "payment_id=" + payment_id +
+                ", customer_id=" + customer_id +
+                ", staff_id=" + staff_id +
+                ", rental_id=" + rental_id +
+                ", amount=" + amount +
+                ", payment_date='" + payment_date + '\'' +
+                '}';
+    }
+}
